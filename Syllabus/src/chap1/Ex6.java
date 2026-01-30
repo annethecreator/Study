6de20dd246de9cb5b4e6d5359bcb8de4319aacc7
58876 <58876@etu.he2b.be>
2026-01-30 14:54:36 +0100

    Ex 6 tva

--- Syllabus/src/chap1/Ex6.java
+++ Syllabus/src/chap1/Ex6.java
@@ -7,10 +7,15 @@ import java.util.Scanner;
 public class Ex6 {
 public static void main(String[] args) {
 	Scanner clavier = new Scanner(System.in);
-	System.out.println("Entre le prix htva");
-	double  prixhtva = clavier.nextDouble();
-	System.out.println("Entre le taux de tva");
-	double taux = clavier.nextDouble();
-	
+	//calcul de la tva a ajouter un peu plustard
+	System.out.println("Entre le prix ");
+	double phtva = clavier.nextDouble();
+	System.out.println("entre le taux de tva");
+	double taux =clavier.nextDouble();
+	double tva = phtva*(taux/100);
+	//calcul prixttc 
+	System.out.println("Prix avec tva vaut");
+	double ttc = (phtva+tva);
+	System.out.println(ttc);
 }
 }
